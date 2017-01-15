b4a82bf7976498e97050c84ee28152aaff8f7a14
calebsandfort <caleb.sandfort@gmail.com>
2017-01-15 13:17:51 -0800

    Change momentum indicator

--- QuantConnect/FaberMonthlySectorRotation.cs
+++ QuantConnect/FaberMonthlySectorRotation.cs
@@ -9,9 +9,10 @@ namespace QuantConnect.Algorithm.Me
 {
     public class FaberMonthlySectorRotation : QCAlgorithm
     {
+        #region Properties
         bool extraCharting = false;
-        DateTime LastRotationTime = new DateTime(1980, 1, 1);
-        TimeSpan RotationInterval = TimeSpan.FromDays(30);
+        int currentMonth = -1;
+        DateTime lastProcessedDate = DateTime.MinValue;
 
         private Symbol spy = QuantConnect.Symbol.Create("SPY", SecurityType.Equity, Market.USA);
         SimpleMovingAverage spySma = null;
@@ -19,14 +20,16 @@ namespace QuantConnect.Algorithm.Me
         //QuantConnect.ToolBox.exe "XLI,XLB,XLE,XLV,XLP,XLU,XLF,XLY,XLK,SPY" Daily 19980101 20161231
         List<string> SectorEtfSymbols = new List<string> { "XLI", "XLB", "XLE", "XLV", "XLP", "XLU", "XLF", "XLY", "XLK" };
         List<SymbolData> SectorEtfs = new List<SymbolData>();
+        #endregion
 
+        #region Initialize
         public override void Initialize()
         {
             SetCash(10000);
-            SetStartDate(2000, 1, 1);
+            SetStartDate(2010, 1, 1);
             SetEndDate(2016, 12, 31);
 
-            AddEquity(spy.ID.Symbol, Resolution.Daily);
+            AddEquity(spy.ID.Symbol, Resolution.Minute);
             spySma = SMA(spy, 200, Resolution.Daily);
 
             var history = History(spy, 201, Resolution.Daily);
@@ -36,14 +39,14 @@ namespace QuantConnect.Algorithm.Me
                 spySma.Update(tradeBar.EndTime, tradeBar.Close);
             }
 
-            SetBenchmark("SPY");
+            SetBenchmark(spy);
 
             foreach (var sym in SectorEtfSymbols)
             {
                 //Symbol symbol = QuantConnect.Symbol.Create(sym, SecurityType.Equity, Market.USA);
 
-                AddSecurity(SecurityType.Equity, sym, Resolution.Daily);
-                var threeMonthPerformance = MOM(sym, 60, Resolution.Daily);
+                AddSecurity(SecurityType.Equity, sym, Resolution.Minute);
+                var threeMonthPerformance = MOMP(sym, 60, Resolution.Daily);
 
                 history = History(sym, 61, Resolution.Daily);
 
@@ -59,6 +62,11 @@ namespace QuantConnect.Algorithm.Me
                 });
             }
 
+            Schedule.On(DateRules.EveryDay("SPY"), TimeRules.AfterMarketOpen(spy, 1), () =>
+            {
+                Allocate();
+            });
+
             #region Charting
             if (extraCharting)
             {
@@ -73,60 +81,91 @@ namespace QuantConnect.Algorithm.Me
                 Series buyingAllowedSeries = new Series("Buying Allowed", SeriesType.Line, 1);
                 stockPlot.AddSeries(buyingAllowedSeries);
 
-                AddChart(stockPlot); 
+                AddChart(stockPlot);
             }
             #endregion
         }
+        #endregion
 
-        public void OnData(TradeBars data)
+        public void Allocate()
         {
-            if (!this.IsWarmingUp)
+            //TradeBar spyBar = data[spy];
+
+            //if (extraCharting)
+            //{
+            //    Plot("SPY", "Price", data[spy].Close);
+            //    Plot("SPY", "SMA", spySma);
+            //    Plot("SPY", "Buying Allowed", data[spy].Close > spySma ? 1 : -1);
+            //}
+
+            if (currentMonth != Time.Month)
             {
-                TradeBar spyBar = data[spy];
+                Log(String.Format("Allocate: {0}", Time.ToShortDateString()));
 
-                if (extraCharting)
+                TradeBar spyLast = History(spy, 1, Resolution.Daily).Last();
+
+                currentMonth = Time.Month;
+
+                if (spyLast.Close > spySma)
                 {
-                    Plot("SPY", "Price", data[spy].Close);
-                    Plot("SPY", "SMA", spySma);
-                    Plot("SPY", "Buying Allowed", data[spy].Close > spySma ? 1 : -1); 
+                    List<String> topPerformers = this.SectorEtfs.OrderByDescending(x => x.ThreeMonthPerformance).Select(x => x.Symbol).Take(3).ToList();
+
+                    foreach (Symbol x in Portfolio.Keys)
+                    {
+                        if (Portfolio[x].Invested && !topPerformers.Contains(x))
+                        {
+                            Liquidate(x);
+                        }
+                    }
+
+                    foreach (String x in topPerformers)
+                    {
+                        SetHoldings(x, .333);
+                    }
                 }
-
-                var delta = Time.Subtract(LastRotationTime);
-                if (delta > RotationInterval)
+                else
                 {
-                    LastRotationTime = Time;
-
-                    if (data[spy].Close > spySma)
-                    {
-                        Log(String.Format("Buying: {0}", Time.ToShortDateString()));
-
-                        List<String> topPerformers = this.SectorEtfs.OrderByDescending(x => x.ThreeMonthPerformance).Select(x => x.Symbol).Take(3).ToList();
-
-                        foreach (Symbol x in Portfolio.Keys)
-                        {
-                            if (Portfolio[x].Invested && !topPerformers.Contains(x))
-                            {
-                                Liquidate(x);
-                            }
-                        }
-
-                        foreach (String x in topPerformers)
-                        {
-                            SetHoldings(x, .333);
-                        }
-                    }
-                    else
-                    {
-                        Liquidate();
-                    }
+                    Liquidate();
                 }
             }
         }
+
+        #region OnData
+        public void OnData(TradeBars data)
+        {
+            
+        }
+        #endregion
+
+        #region InitPerformanceChart
+        public void InitPerformanceChart()
+        {
+            Chart chart = new Chart("Performance");
+
+            Series spySeries = new Series("SPY", SeriesType.Line, 0);
+            chart.AddSeries(spySeries);
+
+            Series portfolioSeries = new Series("Portfolio", SeriesType.Line, 0);
+            chart.AddSeries(portfolioSeries);
+
+            Series exposure = new Series("Exposure", SeriesType.Line, 1);
+            chart.AddSeries(exposure);
+
+            AddChart(chart);
+        }
+        #endregion
+
+        #region PlotPerformanceChart
+        public void PlotPerformanceChart(TradeBar spyBar)
+        {
+            Plot("Performance", "SPY", spyBar.Close);
+        }
+        #endregion
     }
 
     class SymbolData
     {
         public String Symbol;
-        public Momentum ThreeMonthPerformance { get; set; }
+        public MomentumPercent ThreeMonthPerformance { get; set; }
     }
 }
